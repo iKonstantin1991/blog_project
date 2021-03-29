b3af6d7f76013166c76ed3ac96426ffe3a6a4114
Константин <ikonstantin1991@mail.ru>
2021-03-29 21:49:59 +0300

    deleted comment

--- posts/views.py
+++ posts/views.py
@@ -185,7 +185,6 @@ def profile_follow(request, username):
 def profile_unfollow(request, username):
     author = get_object_or_404(User, username=username)
     get_object_or_404(Follow, user=request.user, author=author).delete()
-    #  Follow.objects.get(user=request.user, author=author).delete()
     return redirect(
         'posts:profile',
         username=username,
